267c696d0c611abcaa1bf8cd130d078e12817052
testsnake <testsnake12@gmail.com>
2023-05-18 11:47:06 -0700

    Update env.js

--- env.js
+++ env.js
@@ -15,4 +15,6 @@ module.exports = {
     OAUTH_CLIENTID: process.env.OAUTH_CLIENTID,
     OAUTH_CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
     OAUTH_REFRESH_TOKEN: process.env.OAUTH_REFRESH_TOKEN,
+    CHATGPT_API_KEY: process.env.CHATGPT_API_KEY,
+
 }
